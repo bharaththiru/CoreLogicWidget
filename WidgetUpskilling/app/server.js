1e32466b9bdb857a551f1e4fd01b8e8869fcfd5d
bharaththiru <bharathnzmobile@gmail.com>
2024-05-02 12:53:26 +1200

    API CALL WORKING Corrected propertyID, renewed token, corrected coreDetailsURL

--- WidgetUpskilling/app/server.js
+++ WidgetUpskilling/app/server.js
@@ -5,13 +5,13 @@ const app = express();
 // Replace with your actual values
 const clientId = 'HMo1dGwAnWNQbyGejWswyNuE51A8izko';
 const clientSecret = 'zvTCkMvDcOa8bqQP';
-const accessToken = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJDUlQiLCJNQVAiLCJQVFkiLCJTR1QiLCJTVFMiLCJUVEwiXSwicm9sZXMiOltdLCJpc3MiOiJodHRwczovL2FjY2Vzcy1hcGkuY29yZWxvZ2ljLmFzaWEiLCJlbnZfYWNjZXNzX3Jlc3RyaWN0Ijp0cnVlLCJleHAiOjE3MTQ1Nzc2NjksImdlb19jb2RlcyI6WyJBQ1QgLSBGdWxsIFN0YXRlIiwiTlNXIC0gTWV0cm8iLCJOU1cgLSBSZWdpb25hbCIsIk5UIC0gRnVsbCBTdGF0ZSIsIlFMRCAtIE1ldHJvIiwiUUxEIC0gUmVnaW9uYWwiLCJTQSAtIE1ldHJvIiwiU0EgLSBSZWdpb25hbCIsIlRBUyAtIEZ1bGwgU3RhdGUiLCJWSUMgLSAoQUEpIEZ1bGwgU3RhdGUiLCJWSUMgLSBGdWxsIFN0YXRlIiwiVklDIC0gTWV0cm8iLCJWSUMgLSBSZWdpb25hbCIsIldBIC0gTWV0cm8iLCJXQSAtIFJlZ2lvbmFsIiwiTm9ydGggSXNsYW5kIiwiU291dGggSXNsYW5kIl0sImNsaWVudF9pZCI6IkhNbzFkR3dBbldOUWJ5R2VqV3N3eU51RTUxQThpemtvIiwic291cmNlX2V4Y2x1c2lvbiI6W119.vcU6OCGo2SeB1AErbWqOlbXQO-8siuj5DvVFLdVz4phg1yRF7GkiKbyJZjUNlppMMVDp7o_sgyKhjRODkl27N8qCDrHcynfH69IPCM5BMdekgLIAE0XplEATdw5f3bQeoUs2l-EwDM6IKBYa6wZLs5l_0FHff4WwjNEX6yQgfWE`;
-//const propertyId = '47872329'; // Replace with your search bar input variable
-//const streetName = `Margaret Street`
+const accessToken = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJDUlQiLCJNQVAiLCJQVFkiLCJTR1QiLCJTVFMiLCJUVEwiXSwicm9sZXMiOltdLCJpc3MiOiJodHRwczovL2FjY2Vzcy1hcGkuY29yZWxvZ2ljLmFzaWEiLCJlbnZfYWNjZXNzX3Jlc3RyaWN0Ijp0cnVlLCJleHAiOjE3MTQ2NTMzNTgsImVudiI6InNhbmRib3giLCJnZW9fY29kZXMiOlsiQUNUIC0gRnVsbCBTdGF0ZSIsIk5TVyAtIE1ldHJvIiwiTlNXIC0gUmVnaW9uYWwiLCJOVCAtIEZ1bGwgU3RhdGUiLCJRTEQgLSBNZXRybyIsIlFMRCAtIFJlZ2lvbmFsIiwiU0EgLSBNZXRybyIsIlNBIC0gUmVnaW9uYWwiLCJUQVMgLSBGdWxsIFN0YXRlIiwiVklDIC0gKEFBKSBGdWxsIFN0YXRlIiwiVklDIC0gRnVsbCBTdGF0ZSIsIlZJQyAtIE1ldHJvIiwiVklDIC0gUmVnaW9uYWwiLCJXQSAtIE1ldHJvIiwiV0EgLSBSZWdpb25hbCIsIk5vcnRoIElzbGFuZCIsIlNvdXRoIElzbGFuZCJdLCJjbGllbnRfaWQiOiJITW8xZEd3QW5XTlFieUdlaldzd3lOdUU1MUE4aXprbyIsInNvdXJjZV9leGNsdXNpb24iOltdfQ.TYCHyPM8b9l2ev9xbQPIW7bWLyHI7CvVEO-VIYE0doE7sX6BpEKdXQ_jkMUxJAV6RKVjt0RuD_pWtwbk1t066HaSVJZ7VbKupevXEThLCCGeeGwSHQ10fdF0sUoKum8kmzpLmqxVldL3Fo10ytlpNyjF3WtiZ8feRulAqFbJlLU`;
+const propertyId = '47872329'; // Replace with your search bar input variable
 
 // Construct the CoreLogic API URL (replace with the specific endpoint you're using)
+
 //const locationURL = `https://api-sbox.corelogic.asia/property-details/nz/properties/${propertyId}/location`; //location url
-//const coreDetailsURL = `https://api-sbox.corelogic.asia/property-details/nz/properties/${propertyId}/attributes/core`; //core details url - beds, baths, etc.
+const coreDetailsURL = `https://api-sbox.corelogic.asia/property-details/au/properties/${propertyId}/attributes/core`; //core details url - beds, baths, etc.
 
 const suggestionURL = `https://api-uat.corelogic.asia/sandbox/property/nz/v2/suggest.json?q=Margaret%20Street` //Specific url for testing
 
@@ -22,15 +22,13 @@ const options = {
   };
 
 app.get('/api/location', (req, res) => {
-  axios.get(suggestionURL, options)
+  axios.get(coreDetailsURL, options)
    .then(response => {
 
     // Set the Access-Control-Allow-Origin header to allow the client-side to access the response
     res.setHeader('Access-Control-Allow-Origin', 'https://127.0.0.1:5000'); // Replace with your actual client-side origin
     res.json(response.data);
-
-      // Process the response data (property details)
-      res.json(response.data);
+    
     })
    .catch(error => {
       console.error('Error fetching property details:', error);

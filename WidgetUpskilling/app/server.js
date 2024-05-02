25aec74487b7b26598bec4b8a881bc33e3adf0d3
bharaththiru <bharathnzmobile@gmail.com>
2024-05-02 13:07:19 +1200

    added IDs Call URL to recieve all IDs of Suggested Properties

--- WidgetUpskilling/app/server.js
+++ WidgetUpskilling/app/server.js
@@ -9,8 +9,15 @@ const clientSecret = 'zvTCkMvDcOa8bqQP';
 //Token Expiry: 12 hours
 const accessToken = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJDUlQiLCJNQVAiLCJQVFkiLCJTR1QiLCJTVFMiLCJUVEwiXSwicm9sZXMiOltdLCJpc3MiOiJodHRwczovL2FjY2Vzcy1hcGkuY29yZWxvZ2ljLmFzaWEiLCJlbnZfYWNjZXNzX3Jlc3RyaWN0Ijp0cnVlLCJleHAiOjE3MTQ2NTMzNTgsImVudiI6InNhbmRib3giLCJnZW9fY29kZXMiOlsiQUNUIC0gRnVsbCBTdGF0ZSIsIk5TVyAtIE1ldHJvIiwiTlNXIC0gUmVnaW9uYWwiLCJOVCAtIEZ1bGwgU3RhdGUiLCJRTEQgLSBNZXRybyIsIlFMRCAtIFJlZ2lvbmFsIiwiU0EgLSBNZXRybyIsIlNBIC0gUmVnaW9uYWwiLCJUQVMgLSBGdWxsIFN0YXRlIiwiVklDIC0gKEFBKSBGdWxsIFN0YXRlIiwiVklDIC0gRnVsbCBTdGF0ZSIsIlZJQyAtIE1ldHJvIiwiVklDIC0gUmVnaW9uYWwiLCJXQSAtIE1ldHJvIiwiV0EgLSBSZWdpb25hbCIsIk5vcnRoIElzbGFuZCIsIlNvdXRoIElzbGFuZCJdLCJjbGllbnRfaWQiOiJITW8xZEd3QW5XTlFieUdlaldzd3lOdUU1MUE4aXprbyIsInNvdXJjZV9leGNsdXNpb24iOltdfQ.TYCHyPM8b9l2ev9xbQPIW7bWLyHI7CvVEO-VIYE0doE7sX6BpEKdXQ_jkMUxJAV6RKVjt0RuD_pWtwbk1t066HaSVJZ7VbKupevXEThLCCGeeGwSHQ10fdF0sUoKum8kmzpLmqxVldL3Fo10ytlpNyjF3WtiZ8feRulAqFbJlLU`;
 
+
 const propertyId = '47872329'; // Hard coding property ID to test API call. Need to use search bar widget input. 
+const address = `Margaret Street`
+
+
 const coreDetailsURL = `https://api-sbox.corelogic.asia/property-details/au/properties/${propertyId}/attributes/core`; //core details url - beds, baths, etc.
+
+const getIDsURL = `https://api-uat.corelogic.asia/sandbox/property/au/v2/suggest.json?q=${address}`; //Need to extract LocationID using this data retrieved from this call
+//Use locationID from to get propertyID
 
 
 
@@ -21,7 +28,7 @@ const options = {
   };
 
 app.get('/api/location', (req, res) => {
-  axios.get(coreDetailsURL, options)
+  axios.get(getIDsURL, options)
    .then(response => {
 
     // Set the Access-Control-Allow-Origin header to allow the client-side to access the response

46e1d97c68157761f72e98d811b31731a4fe0817
bharaththiru <bharathnzmobile@gmail.com>
2024-05-07 11:33:36 +1200

    Make API call using search bar - Address Search

--- WidgetUpskilling/app/script.js
+++ WidgetUpskilling/app/script.js
@@ -1,13 +1,49 @@
-// Replace with your actual values
-const propertyId = '353657253'; // Replace with your search bar input variable
+//const { response } = require("express");
 
-// Make a request to the server-side script
-fetch(`http://localhost:3000/api/location?propertyId=${propertyId}`)
-.then(response => response.json())
-.then(data => {
-    // Process the response data (property details)
-    console.log(data);
-  })
-.catch(error => {
-    console.error('Error fetching property details:', error);
+// Replace with your actual values
+const propertyId = '47872329'; // Replace with your search bar input variable
+
+// // Make a request to the server-side script
+// fetch(`http://localhost:3000/api/location?propertyId=${propertyId}`)
+// .then(response => response.json())
+// .then(data => {
+//     // Process the response data (property details)
+//     console.log(data);
+//   })
+// .catch(error => {
+//     console.error('Error fetching property details:', error);
+//   });
+
+
+
+// //Search Bar Functionality
+// document.addEventListener('DOMContentLoaded', (event) => {
+
+//   document.querySelector('.search-button').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     userInput = document.getElementById('search_bar').value;
+//     console.log(userInput);
+//   });
+
+// });
+
+
+
+
+//Search Bar Functionality
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  document.querySelector('.search-button').addEventListener('click', function(event) {
+    event.preventDefault();
+    const userInput = document.getElementById('search_bar').value;
+    fetch(`http://localhost:3000/api/location?address=${userInput}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error fetching property details:', error);
+    });
   });
+
+});
--- WidgetUpskilling/app/server.js
+++ WidgetUpskilling/app/server.js
@@ -1,23 +1,19 @@
 const axios = require('axios');
 const express = require('express');
+const qs = require('querystring');
 const app = express();
 
-// Replace with your actual values
 const clientId = 'HMo1dGwAnWNQbyGejWswyNuE51A8izko';
 const clientSecret = 'zvTCkMvDcOa8bqQP';
 
 //Token Expiry: 12 hours
-const accessToken = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJDUlQiLCJNQVAiLCJQVFkiLCJTR1QiLCJTVFMiLCJUVEwiXSwicm9sZXMiOltdLCJpc3MiOiJodHRwczovL2FjY2Vzcy1hcGkuY29yZWxvZ2ljLmFzaWEiLCJlbnZfYWNjZXNzX3Jlc3RyaWN0Ijp0cnVlLCJleHAiOjE3MTQ2NTMzNTgsImVudiI6InNhbmRib3giLCJnZW9fY29kZXMiOlsiQUNUIC0gRnVsbCBTdGF0ZSIsIk5TVyAtIE1ldHJvIiwiTlNXIC0gUmVnaW9uYWwiLCJOVCAtIEZ1bGwgU3RhdGUiLCJRTEQgLSBNZXRybyIsIlFMRCAtIFJlZ2lvbmFsIiwiU0EgLSBNZXRybyIsIlNBIC0gUmVnaW9uYWwiLCJUQVMgLSBGdWxsIFN0YXRlIiwiVklDIC0gKEFBKSBGdWxsIFN0YXRlIiwiVklDIC0gRnVsbCBTdGF0ZSIsIlZJQyAtIE1ldHJvIiwiVklDIC0gUmVnaW9uYWwiLCJXQSAtIE1ldHJvIiwiV0EgLSBSZWdpb25hbCIsIk5vcnRoIElzbGFuZCIsIlNvdXRoIElzbGFuZCJdLCJjbGllbnRfaWQiOiJITW8xZEd3QW5XTlFieUdlaldzd3lOdUU1MUE4aXprbyIsInNvdXJjZV9leGNsdXNpb24iOltdfQ.TYCHyPM8b9l2ev9xbQPIW7bWLyHI7CvVEO-VIYE0doE7sX6BpEKdXQ_jkMUxJAV6RKVjt0RuD_pWtwbk1t066HaSVJZ7VbKupevXEThLCCGeeGwSHQ10fdF0sUoKum8kmzpLmqxVldL3Fo10ytlpNyjF3WtiZ8feRulAqFbJlLU`;
-
+const accessToken = `eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJDUlQiLCJNQVAiLCJQVFkiLCJTR1QiLCJTVFMiLCJUVEwiXSwicm9sZXMiOltdLCJpc3MiOiJodHRwczovL2FjY2Vzcy1hcGkuY29yZWxvZ2ljLmFzaWEiLCJlbnZfYWNjZXNzX3Jlc3RyaWN0Ijp0cnVlLCJleHAiOjE3MTUwNzgzOTMsImVudiI6InNhbmRib3giLCJnZW9fY29kZXMiOlsiQUNUIC0gRnVsbCBTdGF0ZSIsIk5TVyAtIE1ldHJvIiwiTlNXIC0gUmVnaW9uYWwiLCJOVCAtIEZ1bGwgU3RhdGUiLCJRTEQgLSBNZXRybyIsIlFMRCAtIFJlZ2lvbmFsIiwiU0EgLSBNZXRybyIsIlNBIC0gUmVnaW9uYWwiLCJUQVMgLSBGdWxsIFN0YXRlIiwiVklDIC0gKEFBKSBGdWxsIFN0YXRlIiwiVklDIC0gRnVsbCBTdGF0ZSIsIlZJQyAtIE1ldHJvIiwiVklDIC0gUmVnaW9uYWwiLCJXQSAtIE1ldHJvIiwiV0EgLSBSZWdpb25hbCIsIk5vcnRoIElzbGFuZCIsIlNvdXRoIElzbGFuZCJdLCJjbGllbnRfaWQiOiJITW8xZEd3QW5XTlFieUdlaldzd3lOdUU1MUE4aXprbyIsInNvdXJjZV9leGNsdXNpb24iOltdfQ.YSvypDBfi3uWYgtEtwQc5quHcklexamevxt7Kox1RF7pK-dRlFpoadaNpbYuCaURlC5UgM1c2kmBNU_n8Mvr-BQR94fyYHY78VMKBT_Un-RDxGcJZ4JOyW-uUQIYOHvzGaddj5Q1QkN866WdmOw5yWU2wWsX24P1PNgOfSx886g`;
 
 const propertyId = '47872329'; // Hard coding property ID to test API call. Need to use search bar widget input. 
-const address = `Margaret Street`
 
+const authURL = `https://api-sbox.corelogic.asia/access/oauth/token`;
 
 const coreDetailsURL = `https://api-sbox.corelogic.asia/property-details/au/properties/${propertyId}/attributes/core`; //core details url - beds, baths, etc.
-
-const getIDsURL = `https://api-uat.corelogic.asia/sandbox/property/au/v2/suggest.json?q=${address}`; //Need to extract LocationID using this data retrieved from this call
-//Use locationID from to get propertyID
 
 
 
@@ -27,7 +23,12 @@ const options = {
     }
   };
 
+
+//Fetching location details of property - to extract propertyID
 app.get('/api/location', (req, res) => {
+  const address = req.query.address;
+  const getIDsURL = `https://api-uat.corelogic.asia/sandbox/property/au/v2/suggest.json?q=${address}`; //Need to extract LocationID using this data retrieved from this call
+  
   axios.get(getIDsURL, options)
    .then(response => {
 
@@ -41,8 +42,8 @@ app.get('/api/location', (req, res) => {
       res.status(500).json({ error: 'Error fetching property details' });
     });
 
-
 });
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');

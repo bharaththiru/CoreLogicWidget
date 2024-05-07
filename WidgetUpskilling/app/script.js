b2da321ef01ac2f50d28903cabaff6afe4b17a7b
bharaththiru <bharathnzmobile@gmail.com>
2024-05-07 15:03:53 +1200

    Displaying Search Results - Extracting propertyId if exists

--- WidgetUpskilling/app/script.js
+++ WidgetUpskilling/app/script.js
@@ -1,49 +1,43 @@
-//const { response } = require("express");
+var addressSuggestions;
+var propertyId;
 
-// Replace with your actual values
-const propertyId = '47872329'; // Replace with your search bar input variable
-
-// // Make a request to the server-side script
-// fetch(`http://localhost:3000/api/location?propertyId=${propertyId}`)
-// .then(response => response.json())
-// .then(data => {
-//     // Process the response data (property details)
-//     console.log(data);
-//   })
-// .catch(error => {
-//     console.error('Error fetching property details:', error);
-//   });
-
-
-
-// //Search Bar Functionality
-// document.addEventListener('DOMContentLoaded', (event) => {
-
-//   document.querySelector('.search-button').addEventListener('click', function(event) {
-//     event.preventDefault();
-//     userInput = document.getElementById('search_bar').value;
-//     console.log(userInput);
-//   });
-
-// });
-
-
-
-
-//Search Bar Functionality
+// Search Bar Functionality
 document.addEventListener('DOMContentLoaded', (event) => {
 
   document.querySelector('.search-button').addEventListener('click', function(event) {
     event.preventDefault();
     const userInput = document.getElementById('search_bar').value;
     fetch(`http://localhost:3000/api/location?address=${userInput}`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.error('Error fetching property details:', error);
-    });
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        addressSuggestions = data.suggestions;
+        console.log(addressSuggestions);
+        displaySuggestions(addressSuggestions); // Call the function directly with fetched data
+      })
+      .catch(error => {
+        console.error('Error fetching property details:', error);
+      });
   });
+
+  function displaySuggestions(suggestions) {
+    const suggestionList = document.getElementById('search_results');
+
+    suggestionList.innerHTML = ''; // Clearing existing suggestions
+
+    suggestions.forEach(suggestion => {
+      const suggestionItem = document.createElement('li');
+      suggestionItem.textContent = suggestion.suggestion;
+      suggestionItem.addEventListener('click', () => {
+        console.log('User Selected: ', suggestion.suggestion);
+
+        if(suggestion.suggestionType === 'address') {
+          propertyId = suggestion.propertyId;
+          console.log(propertyId);
+        }
+      });
+      suggestionList.appendChild(suggestionItem);
+    });
+  }
 
 });
